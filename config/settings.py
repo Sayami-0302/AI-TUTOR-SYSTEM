7d9f63c5a5c3bcd534a55c6c96233d6d99219834
Pranaya Shrestha <sthapranaya890@gmail.com>
2026-07-27 13:51:44 +0545

    With apps structure and env var completed stage 0 without setting up gemini api key

--- config/settings.py
+++ config/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-80#%!uhmk1t%1uyf6zq994(h$54^9y4k39syb+z@lcdm($z_e)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
