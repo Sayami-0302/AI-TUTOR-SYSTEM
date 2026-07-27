9d6b3d980f49ca4ac608be6bdcffc9c27c695d11
Pranaya Shrestha <sthapranaya890@gmail.com>
2026-07-27 14:35:57 +0545

    Created Abstract User module

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    
+
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["username","full_name"]
+
+    def __str__(self):
+        return self.email
